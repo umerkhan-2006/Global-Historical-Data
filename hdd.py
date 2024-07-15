15acb05bd6d45d33957bbeefd4b573f26da3c906
Umer Khan <umerkhan21.2006@gmail.com>
2024-07-15 17:40:17 +0500

    Kuwait

--- hdd.py
+++ hdd.py
@@ -28,7 +28,7 @@ def save_to_csv(df, stock_symbol):
     print(f'Data saved to {file_name}')
 
 if __name__ == "__main__":
-    stock_symbols = ['^LUXX', 'RTL.LU', 'SESGL.LU', 'SUB.LU', 'BIL.LU', 'STERN.LU']  # Add more symbols as needed (LuxX)
+    stock_symbols = [^PR, NBK.KW, ZAIN.KW, KFIN.KW, BOUBYAN.KW, GIG.KW, KIPCO.KW, AUB.KW, MABANEE.KW, KFH.KW]  # Add more symbols as needed (Kuwait)
     for symbol in stock_symbols:
         try:
             df = get_stock_data(symbol)
